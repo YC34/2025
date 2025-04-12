a9a6979a2453c57f9861f224f2e8a3fe9a80b17b
PYC <rornf0322@naver.com>
2025-04-12 22:36:31 +0900

    [init] auth service

---
+++ auth/README.md
@@ -0,0 +1,4 @@
+# MSA-AUTH Service
+### Spring Security , Jwt, 권한 등에 관한 서비스
+### DB connection config나 어떠한 common config는 이쪽이 기준이 된다.
+## TODO MSA 아키텍쳐이면 각 서비스 별 config가 달라져야 하는가? 
---
+++ auth/src/test/java/com/yc89/auth/AuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yc89.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
