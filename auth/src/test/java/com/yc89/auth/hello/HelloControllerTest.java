70188389f05558dcf71fe6914a44596b152158f0
PYC <rornf0322@naver.com>
2025-04-17 22:31:57 +0900

    TDD Start

---
+++ auth/src/test/java/com/yc89/auth/hello/HelloControllerTest.java
@@ -0,0 +1,30 @@
+package com.yc89.auth.test;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
+@WebMvcTest(controllers = HelloController.class)
+@WithMockUser
+public class HelloControllerTest {
+
+    @Autowired
+    MockMvcTester mockMvcTester;
+
+    // Test의 패키지 구조는   org.junit.jupiter.api를 써야한다.
+    @Test
+    public void hello(){
+        mockMvcTester.get()
+                .assertThat()
+                .apply(print())
+                .bodyText()
+                .isEqualTo("Hello World");
+    }
+
+
+}
